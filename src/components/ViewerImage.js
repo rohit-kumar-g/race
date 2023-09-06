969c751bf44978a9648089be96eee0b145ed8dba
rohitkumar <rohitkr828204@gmail.com>
2023-09-06 22:43:40 +0530

    rev 3.0

--- src/components/ViewerImage.js
+++ src/components/ViewerImage.js
@@ -1,59 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { BsArrowsFullscreen, BsFullscreenExit } from "react-icons/bs";
 import { Carousel } from "react-responsive-carousel";
-import "./ViewerImage.css"; // Import your CSS styles for fullscreen
+import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
+import "./ViewerImage.css";
+import "lightgallery.js/dist/css/lightgallery.css";
 const ViewerImage = ({ car }) => {
   const [fullscreen, setFullscreen] = useState(false);
-  // const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const toggleFullscreen = () => {
     setFullscreen(!fullscreen);
   };
-  const handleImageClick = (index) => {
-    if (!fullscreen) {
-      // setCurrentImageIndex(index);
-      toggleFullscreen();
-    }
-  };
   useEffect(() => {
     if (fullscreen) {
-      document.body.style.overflow = "hidden"; // Disable body scrolling in fullscreen mode
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "auto"; // Enable body scrolling when exiting fullscreen
+      document.body.style.overflow = "auto";
     }
     return () => {
-      document.body.style.overflow = "auto"; // Ensure body scrolling is enabled when component unmounts
+      document.body.style.overflow = "auto";
     };
   }, [fullscreen]);
-  const centerSlidePercentage = fullscreen ? 60 : 40; // Adjust the centerSlidePercentage based on fullscreen
+  // const centerSlidePercentage = fullscreen ? 60 : 40;
+  const [visible, setVisible] = useState(true);
   return (
     <div className={`viewer-image ${fullscreen ? "fullscreen" : ""}`}>
-      <Carousel
-        swipeable={true}
-        infiniteLoop={true}
-        showThumbs={fullscreen}
-        autoPlay={true}
-        autoPlaySpeed={5000}
-        centerSlidePercentage={centerSlidePercentage}
-        centerMode={true}
-        showIndicators={false}
-      >
-        {car.images.map((image, index) => (
-          <div
-            key={index}
-            onClick={() => handleImageClick(index)} // Handle click to enter fullscreen
+      <div className="content">
+        <LightgalleryProvider>
+          <Carousel
+            swipeable={true}
+            infiniteLoop={true}
+            showThumbs={fullscreen}
+            autoPlay={true}
+            autoPlaySpeed={5000}
+            centerSlidePercentage={40}
+            centerMode={true}
+            showIndicators={false}
           >
-            <img
-              className={`${
-                fullscreen ? "fullscreen_about_car" : "about_car_img"
-              }`}
-              src={image}
-              alt={`Imagfe ${index}`}
-            />
-          </div>
-        ))}
-      </Carousel>
-      <div className="fullscreen-toggle" onClick={toggleFullscreen}>
-        {fullscreen ? <BsFullscreenExit /> : <BsArrowsFullscreen />}
+            {car.images.map((p, idx) => (
+              <LightgalleryItem key={idx} group="group2" src={p}>
+                <img
+                  src={p}
+                  style={{
+                    aspectRatio: "4/3",
+                    maxWidth: "575px",
+                    minHeight: "300px",
+                    width: "100%",
+                  }}
+                />
+              </LightgalleryItem>
+            ))}
+          </Carousel>
+        </LightgalleryProvider>
       </div>
     </div>
   );
